dd9868d9e5f295da8e90c2d681a95feca9659c69
Najim Rahman <44024640+najim11rahman@users.noreply.github.com>
2020-07-26 14:58:01 +0530

    Delete myscripts.js

--- Color input/myscripts.js
+++
@@ -1,9 +0,0 @@
-let p = document.querySelector('p');
-let button = document.querySelector('button');
-let input = document.querySelector('input');
-
-button.addEventListener('click', () => {
-  p.style.backgroundColor = input.value;
-  p.innerHTML = input.value;
-  p.style.color = 'white';
-})
